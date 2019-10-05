04531424ff139cd0320b53ca1704c8e496af8db6
naoketa <49198895+naoketa@users.noreply.github.com>
2019-10-06 01:45:42 +0900

    refactor

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -1,18 +1,17 @@
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Anagram {
 
-  public static List<String> getAnagram(String word) {
+  public static List<String> makeAnagrams(String word) {
     if(word.length() <= 1){
       return Arrays.asList(word);
     }
     List<String> anagrams = new ArrayList<>();
     char[] chars = word.toCharArray();
     for(int i = 0; i< chars.length; i++){
-      for(String str: getAnagram(word.substring(0,i) + word.substring(i+1))){
+      for(String str: makeAnagrams(word.substring(0,i) + word.substring(i+1))){
         anagrams.add(chars[i] + str);
       }
     }
--- src/test/java/AnagramTest.java
+++ src/test/java/AnagramTest.java
@@ -7,25 +7,25 @@ import org.junit.Test;
 public class AnagramTest {
   @Test
   public void extract_anagram_from_a_char(){
-    List<String> actual = Anagram.getAnagram("a");
+    List<String> actual = Anagram.makeAnagrams("a");
     assertThat(actual, hasItems("a"));
   }
 
   @Test
   public void extract_anagram_from_two_chars(){
-    List<String> actual = Anagram.getAnagram("ab");
+    List<String> actual = Anagram.makeAnagrams("ab");
     assertThat(actual, hasItems("ab", "ba"));
   }
 
   @Test
   public void extract_anagram_from_three_chars(){
-    List<String> actual = Anagram.getAnagram("abc");
+    List<String> actual = Anagram.makeAnagrams("abc");
     assertThat(actual, hasItems("abc", "acb", "bac", "bca", "cab", "cba"));
   }
 
   @Test
   public void extract_anagram_from_same_chars(){
-    List<String> actual = Anagram.getAnagram("aab");
+    List<String> actual = Anagram.makeAnagrams("aab");
     assertThat(actual, hasItems("aab", "baa", "aba"));
   }
 
